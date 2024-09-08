803dad920cbe2cce5222982741e205964f552e0d
Krishna Sai Teja Talluri <148251416+teja05443@users.noreply.github.com>
2024-09-08 10:01:25 +0530

    Update Main.py

--- Src/Main.py
+++ Src/Main.py
@@ -34,100 +34,100 @@ def main():
 if __name__ == "__main__":
     main()
 
-# # Detect the disease
-# import boto3
-# import io
-# from PIL import Image, ImageDraw, ImageFont
+# Detect the disease
+import boto3
+import io
+from PIL import Image, ImageDraw, ImageFont
 
-# def display_image(bucket, photo, response):
-#     # Load image from S3 bucket
-#     s3 = boto3.client('s3')
-#     obj = s3.get_object(Bucket=bucket, Key=photo)
-#     stream = io.BytesIO(obj['Body'].read())
-#     image = Image.open(stream)
+def display_image(bucket, photo, response):
+    # Load image from S3 bucket
+    s3 = boto3.client('s3')
+    obj = s3.get_object(Bucket=bucket, Key=photo)
+    stream = io.BytesIO(obj['Body'].read())
+    image = Image.open(stream)
 
-#     # Ready image to draw bounding boxes on it.
-#     imgWidth, imgHeight = image.size
-#     draw = ImageDraw.Draw(image)
+    # Ready image to draw bounding boxes on it.
+    imgWidth, imgHeight = image.size
+    draw = ImageDraw.Draw(image)
 
-#     # calculate and display bounding boxes for each detected custom label
-#     print('Detected custom labels for ' + photo)
-#     for customLabel in response['CustomLabels']:
-#         print('Label: ' + str(customLabel['Name']))
-#         print('Confidence: ' + str(customLabel['Confidence']))
-#         if 'Geometry' in customLabel:
-#             box = customLabel['Geometry']['BoundingBox']
-#             left = imgWidth * box['Left']
-#             top = imgHeight * box['Top']
-#             width = imgWidth * box['Width']
-#             height = imgHeight * box['Height']
+    # calculate and display bounding boxes for each detected custom label
+    print('Detected custom labels for ' + photo)
+    for customLabel in response['CustomLabels']:
+        print('Label: ' + str(customLabel['Name']))
+        print('Confidence: ' + str(customLabel['Confidence']))
+        if 'Geometry' in customLabel:
+            box = customLabel['Geometry']['BoundingBox']
+            left = imgWidth * box['Left']
+            top = imgHeight * box['Top']
+            width = imgWidth * box['Width']
+            height = imgHeight * box['Height']
 
-#             fnt = ImageFont.truetype('arial.ttf', 50)
-#             draw.text((left, top), customLabel['Name'], fill='#00d400', font=fnt)
+            fnt = ImageFont.truetype('arial.ttf', 50)
+            draw.text((left, top), customLabel['Name'], fill='#00d400', font=fnt)
 
-#             print('Left: ' + '{0:.0f}'.format(left))
-#             print('Top: ' + '{0:.0f}'.format(top))
-#             print('Label Width: ' + "{0:.0f}".format(width))
-#             print('Label Height: ' + "{0:.0f}".format(height))
+            print('Left: ' + '{0:.0f}'.format(left))
+            print('Top: ' + '{0:.0f}'.format(top))
+            print('Label Width: ' + "{0:.0f}".format(width))
+            print('Label Height: ' + "{0:.0f}".format(height))
 
-#             points = (
-#                 (left, top),
-#                 (left + width, top),
-#                 (left + width, top + height),
-#                 (left, top + height),
-#                 (left, top))
-#             draw.line(points, fill='#00d400', width=5)
-#         else:
-#             print("No geometry for this label")
+            points = (
+                (left, top),
+                (left + width, top),
+                (left + width, top + height),
+                (left, top + height),
+                (left, top))
+            draw.line(points, fill='#00d400', width=5)
+        else:
+            print("No geometry for this label")
 
-#     image.show()
+    image.show()
 
-# def detect_custom_labels(model_arn, bucket, photo, min_confidence):
-#     rekognition = boto3.client('rekognition')
-#     response = rekognition.detect_custom_labels(
-#         ProjectVersionArn=model_arn,
-#         Image={'S3Object': {'Bucket': bucket, 'Name': photo}},
-#         MinConfidence=min_confidence
-#     )
-#     display_image(bucket, photo, response)
-#     return len(response['CustomLabels'])
+def detect_custom_labels(model_arn, bucket, photo, min_confidence):
+    rekognition = boto3.client('rekognition')
+    response = rekognition.detect_custom_labels(
+        ProjectVersionArn=model_arn,
+        Image={'S3Object': {'Bucket': bucket, 'Name': photo}},
+        MinConfidence=min_confidence
+    )
+    display_image(bucket, photo, response)
+    return len(response['CustomLabels'])
 
-# def main():
-#     bucket = 'custom-labels-console-us-east-2-174350573c'
-#     photo = 'Image4.jpg'
-#     model_arn = 'arn:aws:rekognition:us-east-2:002743134469:project/plant-leaf-disease-detection/version/plant-leaf-disease-detection.2024-03-12T23.30.49/1710266449012'
-#     min_confidence = 95  # Adjust confidence threshold as needed
+def main():
+    bucket = 'custom-labels-console-us-east-2-174350573c'
+    photo = 'Image4.jpg'
+    model_arn = 'arn:aws:rekognition:us-east-2:002743134469:project/plant-leaf-disease-detection/version/plant-leaf-disease-detection.2024-03-12T23.30.49/1710266449012'
+    min_confidence = 95  # Adjust confidence threshold as needed
 
-#     label_count = detect_custom_labels(model_arn, bucket, photo, min_confidence)
-#     print("Custom labels detected:", label_count)
+    label_count = detect_custom_labels(model_arn, bucket, photo, min_confidence)
+    print("Custom labels detected:", label_count)
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 
-# # Stop the Dl Model
-# import boto3
-# import time
+# Stop the Dl Model
+import boto3
+import time
 
-# def stop_model(model_arn):
+def stop_model(model_arn):
 
-#     client=boto3.client('rekognition')
+    client=boto3.client('rekognition')
 
-#     print('Stopping model:' + model_arn)
+    print('Stopping model:' + model_arn)
 
-#     #Stop the model
-#     try:
-#         response=client.stop_project_version(ProjectVersionArn=model_arn)
-#         status=response['Status']
-#         print ('Status: ' + status)
-#     except Exception as e:  
-#         print(e)  
+    #Stop the model
+    try:
+        response=client.stop_project_version(ProjectVersionArn=model_arn)
+        status=response['Status']
+        print ('Status: ' + status)
+    except Exception as e:  
+        print(e)  
 
-#     print('Done...')
+    print('Done...')
     
-# def main():
+def main():
     
-#     model_arn='arn:aws:rekognition:us-east-2:002743134469:project/plant-leaf-disease-detection/version/plant-leaf-disease-detection.2024-03-12T23.30.49/1710266449012'
-#     stop_model(model_arn)
+    model_arn='arn:aws:rekognition:us-east-2:002743134469:project/plant-leaf-disease-detection/version/plant-leaf-disease-detection.2024-03-12T23.30.49/1710266449012'
+    stop_model(model_arn)
 
-# if __name__ == "__main__":
-#     main() 
+if __name__ == "__main__":
+    main() 
